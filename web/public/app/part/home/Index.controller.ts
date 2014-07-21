b95b1bc655e559604d37aca61e16934a312a21ab
Peter Vyvey <peter.vyvey@gmail.com>
2014-07-21 10:28:12 +0200

    show gallaries

--- web/public/app/part/home/Index.controller.ts
+++ web/public/app/part/home/Index.controller.ts
@@ -29,20 +29,36 @@ module Application.Part.Home {
         public isActive: boolean = false;
         public title: string = '';
     }
+
+    export class Gallery {
+        public title: string = '';
+        public description: string = '';
+        public coverPicture: string = '';
+        public pictures : Array<Picture> = new Array<Picture>();
+    }
+
+    export class Picture {
+        public title: string = '';
+        public description: string = '';
+        public fileName: string = '';
+    }
     
     export interface IIndexControllerScope extends ng.IRootScopeService {
         menu: Menu;
+        galleries: Array<Gallery>;
         activateMenuItem(item: MenuItem);
     }
 
     export class IndexControllerScope {
-        static $inject = ['$scope', '$window'];
+        static $inject = ['$scope', '$window', '$http'];
 
-        constructor($scope: any, $window: ng.IWindowService) {
+        constructor($scope: any, $window: ng.IWindowService, $http: ng.IHttpService) {
             this.$scope = this.$localScope = $scope;
             this.$window = $window;
+            this.$http = $http;
 
             this.initializeScope();
+            this.initializeData();
             this.attachEvents();
             this.onResize();
         }
@@ -50,6 +66,7 @@ module Application.Part.Home {
         private $scope: any;
         private $localScope: IIndexControllerScope;
         private $window: ng.IWindowService;
+        private $http: ng.IHttpService;
 
         private menu: Menu;
 
@@ -59,6 +76,8 @@ module Application.Part.Home {
             this.$localScope.menu.addItem(new MenuItem('Portfolio'));
             this.$localScope.menu.addItem(new MenuItem('Services'));
             this.$localScope.menu.addItem(new MenuItem('Contact'));
+
+            this.$localScope.galleries = new Array<Gallery>();
 
             this.$localScope.$on('$routeChangeSuccess', function(event, current) {
                 (<any>$('#fullpage')).fullpage();
@@ -78,12 +97,22 @@ module Application.Part.Home {
             };
         }
 
+        private initializeData(): void {
+            this.$http({method: 'GET', url: '/app/media/gallery/gallery.json'}).
+                success((data, status, headers, config) => {
+                    this.$localScope.galleries = data;
+                    console.log(this.$localScope.galleries);
+                }).
+                error((error, status, headers, config) => {
+                    this.$localScope.galleries.splice(0, this.$localScope.galleries.length);
+                });
+        }
+
         private attachEvents(): void {
             angular.element(this.$window).on('resize', this.onResize);
         }
 
         private onResize(event?: JQueryEventObject):void {
-            console.log('resize', event);
         }
     }
 }
